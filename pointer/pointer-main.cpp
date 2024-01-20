8319f9a155d383b975f6e472a5ca9c2199e9ef64
Rohit Gadhave <rohit.gadhave@viotalworks.com>
2024-01-20 14:14:36 +0530

    pinter

--- pointer/pointer-main.cpp
+++ pointer/pointer-main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+using namespace std;
 
-int main (){
-    int i = 0;
-    std::cout << "ok\n" << i;
-    char sample[] = "GeeksforGeeks";
- 
-    std::cout << sample << " - A computer science portal for geeks";
+struct Rectangle{
+
+};
+
+int main()
+{
+    int iNo = 10;
+    int *iPinter;
+
+    iPinter = &iNo;
+    cout << "size of int " << sizeof(int) << endl;
+    cout << "value of iNo " << iNo << endl;
+    cout << "value of iNo address " << &iNo << endl;
+    cout << "value of iPinter " << iPinter << endl;
+    cout << "value of iPinter value " << *iPinter << endl;
+    cout << "value of iPinter address " << &iPinter << endl;
+    // Allocate Heap memory in c
+
+    int *pointingToHeapInC;
+    pointingToHeapInC = (int *)malloc(iNo * sizeof(int));
+
+    // Allocate Heap memory in cpp
+
+    int *pointingToHeapInCPP;
+    pointingToHeapInCPP = new int[iNo];
+
+    int arr[5];
+    int intSize = sizeof(int);
+
+    for (int i = 0; i < iNo; i++)
+    {
+        cout << i * intSize << " * " << *pointingToHeapInCPP << " - " << pointingToHeapInCPP <<" i " << pointingToHeapInCPP[i] << endl;
+    }
+
+    // for (const p : arr)
+    // {
+    //     cout << p << endl;
+    // }
+
     return 0;
 }
