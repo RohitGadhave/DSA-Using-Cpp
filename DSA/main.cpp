5161ac2faa1565ca6450fa9db55652b2456207bf
Rohit Gadhave <rohitgadhave249@gmail.com>
2024-03-28 11:40:51 +0530

    xcode project added

---
+++ DSA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DSA
+//
+//  Created by Rohit Gadhave Patil on 28/03/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
